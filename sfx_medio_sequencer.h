580ab0529fe166acb18073675d1f8fd1bea757af
enburk <enburk@gmail.com>
2023-11-08 07:30:27 +0300

    video size fix

--- sfx_medio_sequencer.h
+++ sfx_medio_sequencer.h
@@ -98,12 +98,12 @@ namespace sfx::media
 
         void on_change (void* what) override
         {
-            if (what == &coord and
-                coord.was.size !=
-                coord.now.size)
+            if (what == &coord and not fitted)
             {
-                if (not fitted)
-                fit(coord.now.size);
+                for (auto&
+                player: players)
+                player .coord = coord.now.local();
+                players.coord = coord.now.local();
             }
 
             if (what == &players)
